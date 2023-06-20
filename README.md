84a85f5a60f6a6d8e0e9f64a9fcea410ace2cbc3
visheshgubrani <33202551+visheshgubrani@users.noreply.github.com>
2023-06-20 21:06:59 +0530

    Added four operators in js

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # calculator-odin
+Objectives:
+1.Read the value from the button
+2. Display the value 
+3. keep on adding the strings till the value button is being clicked 
+4. Read the value of the operator 
+5. Repeat 1-4 & perform the single calculation and display the result
+6. repeat 1-5 until cleared. 
+7. Should be able to perform calculations after equal to button
+
+
+
+Objective #1
+Read the value from the button
--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const add = numbers.map(function(a, b) {
+function add(a, b) {
     return a + b;
-});
+}
 
-console.log(add);
+function minus(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator;
+
+
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return minus(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    } else {
+        return 'Invalid Operator'
+    }
+}
